5ee2f8a8823ac9f78d0e41e0d3cfd3c9124fe028
gino <gino@alink-group.co.jp>
2020-05-18 15:30:25 +0900

    bash: Update the template script
    * Replace -h handling with a function rather than a string
    * Use getopts to parse the script args/opts
    * Use 3 log levels
    * Remove dependency on another utils.sh

--- bash/template.sh
+++ bash/template.sh
@@ -4,10 +4,15 @@
 #                            DEFINES                                  #
 #######################################################################
 
-PROJECT_ROOT=$(git rev-parse --show-toplevel)
-source ${PROJECT_ROOT}/path/to/utils.sh
+THIS_SCRIPT=$(basename "${BASH_SOURCE[0]}")
+PATH_TO_THIS_SCRIPT=$(readlink -f "${THIS_SCRIPT}")
 
+INFO_COLOR="\e[37m"
+FAIL_COLOR="\e[31m"
+WARN_COLOR="\e[33m"
+RESET_COLOR="\e[0m"
 
+SUDO=$(which sudo)
 
 #######################################################################
 #                           FUNCTIONS                                 #
@@ -15,39 +20,75 @@ source ${PROJECT_ROOT}/path/to/utils.sh
 
 function show_usage()
 {
-    USAGE \
-"Describe the purpose of the script.\n\
-The description can span multiple lines with correct '\n\' placements." \
-        "required|script|parameters" \
-        "--option1: Option1 description" \
-        "--option2: Option2 description" \
-        "--option3: Option3 description"
+    echo "${THIS_SCRIPT} <ARG> [OPTIONS]"
+    echo ""
+    echo "ARG: A required argument"
+    echo ""
+    echo "OPTIONS:"
+    echo ""
+    echo "    -b VAL:   Replace default value with VAL"
+    echo "    -x:       Disable some setting"
+    echo "    -h:       Show this help message"
+    echo ""
 }
 
-function check_environment()
+#######################################################################
+#                            USAGE                                    #
+#######################################################################
+
+function print_info()
 {
-    commands=(apt grep python pip bluetoothctl)
-    check_commands ${commands}
+    printf "${INFO_COLOR}[LOG] $* ${RESET_COLOR}\n"
+}
+
+function print_warn()
+{
+    printf "${WARN_COLOR}[WARN] $* ${RESET_COLOR}\n"
+}
+
+function print_error()
+{
+    printf "${FAIL_COLOR}[ERROR] $* ${RESET_COLOR}\n"
+}
+
+function exit_if_error()
+{
+    if [[ ! $? -eq 0 ]]; then
+        print_error "The last operation failed.\n"
+        exit 1
+    fi
 }
 
 #######################################################################
 #                             MAIN                                    #
 #######################################################################
 
-for mode in "$@"
-do
-    case ${mode} in
-        --help)
-        show_usage
-        ;;
+function main()
+{
+    print_info "Now running ${THIS_SCRIPT}"
+
+    # check
+
+    # do
+
+    # report
+    print_info "${THIS_SCRIPT} is done."
+    print_info "See ${BUILD_OUT}"
+}
+
+DISABLE_SOMETHING=0
+while getopts "hb:x" opt; do
+    case "${opt}" in
+        b)
+            OVERRIDE=${OPTARG}
+            ;;
+        x)
+            DISABLE_SOMETHING=1
+            ;;
         *)
-        ERROR "Unknown option '${mode}'"
-        show_usage
-        ;;
+            show_usage
+            exit 1
+            ;;
     esac
 done
-
-check_environment
-
-# Flow #1: Call other functions here based on the modes passed to the script.
-# Flow #2: Call the functions directly from the case statements then exit N.
+main
